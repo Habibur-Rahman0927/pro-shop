2231c4ced0f1d11816effa748e9d2c531e92ff71
Habibur Rahman <habibur.rahman0927@gmail.com>
2021-10-20 22:26:52 -0700

    frontend and backend added

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: xxxx,
+        isAdmin: true
+    },
+    {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: xxxx,
+    },
+    {
+        name: 'jane Doe',
+        email: 'jane@example.com',
+        password: xxxx,
+    },
+]
